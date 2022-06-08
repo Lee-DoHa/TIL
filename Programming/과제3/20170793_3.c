418a41fb40d80a8abe7fa7313a9412ba2ed09723
Lee-DoHa <johnny550@naver.com>
2022-06-08 20:43:59 +0900

    update 과제3

--- Programming/과제3/20170793_3.c
+++ Programming/과제3/20170793_3.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void){
+	
+	char name[20][10] = {"Sophia", "Olivia", "Riley", "Emma", "Ava", "Isabella", "Aria", "Amelia", "Mia", "Liam", "Noah", "Jackson", "Aiden", "Elijah", "Grayson", "Lucas", "Oliver", "Caden", "Mateo", "David"};
+
+
+	int num[20] = {20220001, 20220010, 20220002, 20220015, 20220009, 20220014, 20220020, 20220005, 20220016, 20220008, 20220012, 20220004, 20220018, 20220017, 20220003, 20220013, 20220007, 20220019, 20220011, 2022000};
+
+
+	int score[20] = {98, 96, 88, 77, 82, 93, 84, 79, 90, 80, 89, 99, 78, 83, 92, 71, 72, 68, 96, 76};
+
+
+	for(int i = 0; i < 20; i++){
+		printf("%s \n", name[i]);
+	}
+	
+	
 	
 	
 	
