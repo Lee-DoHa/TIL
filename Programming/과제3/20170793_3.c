1ff097067d3fc5a2a9f4f60d9946e4350522c812
Lee-DoHa <johnny550@naver.com>
2022-06-09 23:11:54 +0900

    함수1 추가

--- Programming/과제3/20170793_3.c
+++ Programming/과제3/20170793_3.c
@@ -1,6 +1,37 @@
 #include <stdio.h>
 #include <string.h>
 
+void num1(char a[][10], int b[], int c[]){
+	char tmp[10];
+	for(int i = 1; i < 20; i++){
+		for (int j = 0; j < 10; j++){
+			if(strcmp(a[i-1], a[i]) > 0){
+				strcpy(tmp, a[i-1]);
+				strcpy(a[i-1], a[i]);
+				strcpy(a[i], tmp);
+				i = 1;	
+			}
+		}
+	}
+
+	printf("이름       학번       점수\n");
+	for(int i = 0; i < 20; i++){
+		printf("%s   \n", a[i]);
+	}
+}
+
+void num2(char a[][10], int b[], int c[]){
+	
+}
+
+void num3(char a[][10], int b[], int c[]){
+	
+}
+
+void num4(char a[][10], int b[], int c[], char search_name[]){
+	
+}
+
 int main(void){
 	
 	char name[20][10] = {"Sophia", "Olivia", "Riley", "Emma", "Ava", "Isabella", "Aria", "Amelia", "Mia", "Liam", "Noah", "Jackson", "Aiden", "Elijah", "Grayson", "Lucas", "Oliver", "Caden", "Mateo", "David"};
@@ -10,7 +41,33 @@ int main(void){
 
 
 	int score[20] = {98, 96, 88, 77, 82, 93, 84, 79, 90, 80, 89, 99, 78, 83, 92, 71, 72, 68, 96, 76};
+	int n;
+	char search_name[10];
 
+	while(1){
+		printf("1. 이름 오른차순 출력       2. 학번 오름차순 출력\n3. 점수 내림차순 출력       4. 이름 검색\n5. 종료\n\n번호를 선택하세요 : ");
+		scanf("%d", &n);
+		switch(n){
+			case 1:
+				num1(name, num, score);
+				break;
+			case 2:
+                                num2(name, num, score);
+                                break;
+			case 3:
+                                num3(name, num, score);
+                                break;
+			case 4:
+				printf("이름을 입력하세요 : ");
+				scanf("%s", search_name);
+                                num4(name, num, score, search_name);
+                                break;
+			case 5:
+                                printf("$");
+                                break;
+		}
+		
+	}
 
 	for(int i = 0; i < 20; i++){
 		printf("%s \n", name[i]);
