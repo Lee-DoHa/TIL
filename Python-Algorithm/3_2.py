310ee778d77b4353308895834b7f802613394d12
Lee-DoHa <johnny550@naver.com>
2022-06-15 21:55:19 +0900

    Update 3_2.py

--- Python-Algorithm/3_2.py
+++ Python-Algorithm/3_2.py
@@ -10,15 +10,25 @@ second = data[n-2] # 두 번째로 큰 수
 
 result = 0
 
-while True:
-    for i in range(k):
-        if m == 0:
-            break
-        result += first
-        m -= 1
-    if m == 0:
-        break
-    result += second
-    m -= 1
+# while True:
+#     for i in range(k):
+#         if m == 0:
+#             break
+#         result += first
+#         m -= 1
+#     if m == 0:
+#         break
+#     result += second
+#     m -= 1
+
+# print(result)
+
+# 만약 M의 크기가 100억 이상일 때?
+
+count = int(m / (k + 1)) * k
+count += m % (k + 1)
+
+result += (count) * first
+result += (m-count) * second
 
 print(result)
