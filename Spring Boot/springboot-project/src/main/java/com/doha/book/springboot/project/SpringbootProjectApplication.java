3175a423f31d87ce3853635957e84cacb2235107
Lee-DoHa <johnny550@naver.com>
2022-06-16 11:51:34 +0900

    setting

---
+++ Spring Boot/springboot-project/src/main/java/com/doha/book/springboot/project/SpringbootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.doha.book.springboot.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootProjectApplication.class, args);
+	}
+
+}
