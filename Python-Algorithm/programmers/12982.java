fcbcaff94686088d59d31306fa9781f4aa7e68ea
Lee-DoHa <johnny550@naver.com>
2022-10-19 11:03:28 +0900

    Create 12982.java

---
+++ Python-Algorithm/programmers/12982.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+        Arrays.sort(d);
+        int tmp = 0;
+        for(int i = 0; i < d.length; i++) {
+            tmp += d[i];
+            if(tmp <= budget) {
+                answer++;
+                continue;
+            }
+        }
+        return answer;
+    }
+}
