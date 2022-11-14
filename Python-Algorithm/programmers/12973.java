4d3e9df2c27a398aff094742c8200ec955dbeb2e
Lee-DoHa <johnny550@naver.com>
2022-11-14 19:19:56 +0900

    Create 12973.java

---
+++ Python-Algorithm/programmers/12973.java
@@ -0,0 +1,21 @@
+import java.util.Stack;
+
+class Solution
+{
+    public int solution(String s)
+    {
+        int answer = 0;
+        Stack<Character> stack = new Stack<>();
+        
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(!stack.empty() && stack.peek() == c) {
+                stack.pop();
+                continue;
+            }
+            stack.push(c);
+        }
+        
+        return stack.empty() ? 1 : 0;
+    }
+}
